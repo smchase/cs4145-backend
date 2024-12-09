db18b8bb9eeeaca2f678e99362b6e8b624fee87a
Simon Chase <github@simonchase.com>
2024-12-09 16:15:38 +0100

    Full CRUD operations

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import uuid
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 import os
+import uuid
 from urllib.parse import quote_plus
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request, abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 load_dotenv()
 
@@ -30,7 +32,93 @@ def home():
 @app.route("/questions", methods=["GET"])
 def get_questions():
     questions = db.session.execute(db.select(ClassificationQuestion)).scalars()
-    return jsonify([{"query": question.query, "context": question.context, "response": question.response} for question in questions])
+    return jsonify([{"id": question.id, "query": question.query, "context": question.context, "response": question.response} for question in questions])
+
+@app.route("/questions/<string:id>", methods=["GET"])
+def get_question(id):
+    question = db.session.get(ClassificationQuestion, id)
+    if not question:
+        abort(404)
+    return jsonify({
+        "id": question.id,
+        "query": question.query,
+        "context": question.context,
+        "response": question.response
+    })
+
+@app.route("/questions/random", methods=["GET"])
+def get_random_question():
+    question = db.session.execute(
+        db.select(ClassificationQuestion).order_by(func.random()).limit(1)
+    ).scalar()
+    if not question:
+        abort(404)
+    return jsonify({
+        "id": question.id,
+        "query": question.query,
+        "context": question.context,
+        "response": question.response
+    })
+
+@app.route("/questions", methods=["POST"])
+def create_question():
+    if not request.is_json:
+        abort(400)
+    data = request.get_json()
+    
+    required_fields = ["query", "context", "response"]
+    if not all(field in data for field in required_fields):
+        abort(400)
+    
+    question = ClassificationQuestion(
+        query=data["query"],
+        context=data["context"],
+        response=data["response"]
+    )
+    db.session.add(question)
+    db.session.commit()
+    
+    return jsonify({
+        "id": question.id,
+        "query": question.query,
+        "context": question.context,
+        "response": question.response
+    }), 201
+
+@app.route("/questions/<string:id>", methods=["PUT"])
+def update_question(id):
+    if not request.is_json:
+        abort(400)
+    
+    question = db.session.get(ClassificationQuestion, id)
+    if not question:
+        abort(404)
+    
+    data = request.get_json()
+    if "query" in data:
+        question.query = data["query"]
+    if "context" in data:
+        question.context = data["context"]
+    if "response" in data:
+        question.response = data["response"]
+    
+    db.session.commit()
+    return jsonify({
+        "id": question.id,
+        "query": question.query,
+        "context": question.context,
+        "response": question.response
+    })
+
+@app.route("/questions/<string:id>", methods=["DELETE"])
+def delete_question(id):
+    question = db.session.get(ClassificationQuestion, id)
+    if not question:
+        abort(404)
+    
+    db.session.delete(question)
+    db.session.commit()
+    return "", 204
 
 if __name__ == "__main__":
 	app.run(debug=True)
